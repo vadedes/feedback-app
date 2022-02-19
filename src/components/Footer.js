af9f765700ac778f1e5271ae404a044c2617836d
dave <dave@vadedesign.com>
2022-02-19 18:38:29 +0800

    Added basic components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer({ text }) {
+   return (
+      <footer className='Footer'>
+         <div className='container'>
+            <h3>{text}</h3>
+         </div>
+      </footer>
+   );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header({ text }) {
+   return (
+      <header className='header'>
+         <div className='container'>
+            <h3>{text}</h3>
+         </div>
+      </header>
+   );
+}
+
+export default Header;
