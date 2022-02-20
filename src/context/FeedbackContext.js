e6a5384b494c4558ea492527099e412caec284e0
dave <dave@vadedesign.com>
2022-02-20 20:10:19 +0800

    Added feedback edit function and tested.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,6 +22,12 @@ export const FeedbackProvider = ({ children }) => {
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  //add new feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     //we cant just push the new feedback directly since it would replace all the feedback that's currently posted
@@ -29,18 +35,36 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback]);
   };
 
+  //delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete this item?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  //update the feedback data when edited instead of adding a new one
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)));
+  };
+
+  //set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
+        //top portion are states(red), bottom portion are functions(blue)
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
